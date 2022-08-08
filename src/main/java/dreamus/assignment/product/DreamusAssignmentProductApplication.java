d6390a650e89ff3db8284a64bee8681565114811
홍준성 <ghdwnstjd13@gmail.com>
2022-08-08 22:26:44 +0900

    Spring Initializr > git init

---
+++ src/main/java/dreamus/assignment/product/DreamusAssignmentProductApplication.java
@@ -0,0 +1,13 @@
+package dreamus.assignment.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamusAssignmentProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamusAssignmentProductApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dreamus/assignment/product/DreamusAssignmentProductApplicationTests.java
@@ -0,0 +1,13 @@
+package dreamus.assignment.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamusAssignmentProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
